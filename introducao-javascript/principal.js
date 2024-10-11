f5f4aac1118e437247b017eff48af4a157f4e187
Hygor Meireles <160152117+Meireles12@users.noreply.github.com>
2024-10-11 12:16:43 -0300

    Atualização_array

---
+++ introducao-javascript/principal.js
@@ -0,0 +1,11 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+
+var peso = tdPeso.textContent;
+
+console.log(paciente); // tr 
+console.log(tdPeso); // td que tem o peso 
+console.log(peso); // obter 100 
